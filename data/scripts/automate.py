4de1c09bd5fc0b5a8785efa728307cd5345cb034
Ali Ashber <77687615+asshber@users.noreply.github.com>
2022-06-17 11:12:48 +0500

    dumps collection done

--- data/scripts/automate.py
+++ data/scripts/automate.py
@@ -13,12 +13,14 @@ for machine in f:
     os.system('mkdir {0}'.format(machine))
     for services in f[machine]:
             if(services=="ssh"):
+                c=fabric.Connection("{0}@{1}:{2}".format(f[machine]["ssh"]["username"],ip,f[machine]["ssh"]["port"]),connect_kwargs={"password":'{0}'.format(f[machine]["ssh"]["password"])})
+                os.system('sshpass -p {4} scp -r -P {0} ./my_dump_mem root@localhost:/'.format(f[machine][services]["port"],f[machine][services]["username"],ip,machine,f[machine][services]["password"]))
+                c.run('python3 /my_dump_mem.py --pids "$(ps -aux | awk \'{print $2}\')"') #command here
                 os.system('sshpass -p {4} scp -r -P {0} {1}@{2}:/var/log ./{3}'.format(f[machine][services]["port"],f[machine][services]["username"],ip,machine,f[machine][services]["password"]))
             if(services=="db"):
                 os.system('sshpass -p {4} scp -P {0} {1}@{2}:/var/log/db.log ./{3}'.format(f[machine][services]["port"],f[machine][services]["username"],ip,machine,f[machine][services]["password"]))
             if(services=="http"):
                 os.system('sshpass -p {4} scp -r -P {0} {1}@{2}:/etc/ ./{3}'.format(f[machine][services]["port"],f[machine][services]["username"],ip,machine,f[machine][services]["password"]))
             if(machine==""): #edit the machine name here 
-                c=fabric.Connection("{0}@{1}:{2}".format(f[machine]["ssh"]["username"],ip,f[machine]["ssh"]["port"]),connect_kwargs={"password":'{0}'.format(f[machine]["ssh"]["password"])})
                 c.run('ps -aux') #command here
             print("{0} done".format(machine))
